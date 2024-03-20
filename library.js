ef9430ab1e5100d8486918134063d99b57a05af9
Muhammad Hamza Khalid <125656697+mhamza-khalid@users.noreply.github.com>
2024-03-21 01:06:40 +1000

    Added Edit Option

--- library.js
+++ library.js
@@ -6,6 +6,14 @@ let cardsParent = document.querySelector('#cards') ;
 
 let myLibrary = [];   //this array will contain book objects
 
+let editFlag = false;
+let editIndex;
+
+let titleEdit = document.querySelector('#title');
+let authorEdit = document.querySelector('#author');
+let pagesEdit = document.querySelector('#pages');
+let readEdit = document.querySelector('#read');
+
 newBookInput.addEventListener('click', ()=>{
         modal.showModal();
 })
@@ -15,23 +23,66 @@ form.addEventListener('submit', displayBooks);
 
 window.addEventListener('click', (event)=>{ 
 
-        console.log(event.target);
+        // console.log(event.target);
 
         if(event.target.className == 'read'){
-                if(event.target.textContent == "Read Status: Yes"){
-                        event.target.textContent = "Read Status: No";
-                }
-                else if(event.target.textContent == "Read Status: No"){
-                        event.target.textContent = "Read Status: Yes"; 
-                }
+                 if(event.target.textContent == "Read Status: Yes"){
+                         event.target.textContent = "Read Status: No";
+                         myLibrary[event.target.id].read = 'No';
+                        
+                 }
+                 else if(event.target.textContent == "Read Status: No"){
+                         event.target.textContent = "Read Status: Yes"; 
+                         myLibrary[event.target.id].read = 'Yes';
+                 }
+
         }
 
         if(event.target.className == 'delete'){
-                let deleteIndex = event.target.id;
-                myLibrary.splice(deleteIndex,1);
-                displayBooks();
+                let deleteIndex = event.target.id; //the index at which object is to be removed
+                myLibrary.splice(deleteIndex,1);   //remove that object from array
+                displayBooks();                    //display new cards
         }
 
+        if(event.target.className == 'edit'){
+
+                editFlag = true;
+                editIndex = event.target.id;
+
+                console.log(event.target.id)
+
+                modal.showModal();
+
+                
+                //to fill form with existing details of card to be edited
+                titleEdit.value = myLibrary[editIndex].title;
+
+                authorEdit.value = myLibrary[editIndex].author;
+
+                pagesEdit.value = myLibrary[editIndex].pages;
+
+                readEdit.value = myLibrary[editIndex].read;
+
+        }
+
+})
+
+window.addEventListener('submit', ()=>{
+
+        if(editFlag == true){
+                             
+                //to update myLibrary array with edited values
+                myLibrary[editIndex].title = titleEdit.value;
+                myLibrary[editIndex].author = authorEdit.value;
+                myLibrary[editIndex].pages = pagesEdit.value;
+                myLibrary[editIndex].read = readEdit.value; 
+                
+                form.reset();
+
+                editFlag = false;
+                displayBooks();
+                
+         }
 })
 
 function Book(title, author, pages, read){   // Book object constructor used to make the book object
@@ -43,6 +94,8 @@ function Book(title, author, pages, read){   // Book object constructor used to 
 
 }
 function addBookToLibrary(event){
+
+        if (editFlag == false){
         event.preventDefault();
         modal.close();
 
@@ -56,9 +109,13 @@ function addBookToLibrary(event){
         myLibrary.push(book);  // appends book object to myLibrary array
 
         form.reset();   // clear form for next input
+        }
+        else{ return }
 }
 
 function displayBooks(){
+
+     if(editFlag == false){
 
         cardsParent.textContent = "";    //when cards are to be displayed, we remove all existing cards and start displaying cards from start of myLibrary array
         for (let i=0; i < myLibrary.length; i++){
@@ -79,26 +136,29 @@ function displayBooks(){
               let readStatus = document.createElement('button');
               readStatus.textContent = 'Read Status:' + ' ' + myLibrary[i].read;
               readStatus.setAttribute('class', 'read');
+              readStatus.setAttribute('id', i);
 
               let deleteBook = document.createElement('button');
               deleteBook.textContent = 'Delete';
               deleteBook.setAttribute('class', 'delete');
               deleteBook.setAttribute('id', i);
 
-        //       let editBook = document.createElement('button');
-        //       editBook.textContent = 'Edit';
-        //       editBook.setAttribute('class', 'edit');
-        //       editBook.setAttribute('id', i);
+              let editBook = document.createElement('button');
+              editBook.textContent = 'Edit';
+              editBook.setAttribute('class', 'edit');
+              editBook.setAttribute('id', i);
 
               card.appendChild(bookName);    // attach elements to card
               card.appendChild(bookAuthor);
               card.appendChild(bookPages);
               card.appendChild(readStatus);
               card.appendChild(deleteBook);
-        //       card.appendChild(editBook);  //will add edit when I get the time (wasn't a project requirement btw)
+              card.appendChild(editBook);  
 
               cardsParent.appendChild(card);  // attach card to HTML document (cardsParent);
 
               let read = document.querySelectorAll('.read');
         }
+      }
+      else {return}
 }
